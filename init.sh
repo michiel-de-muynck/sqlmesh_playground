2450dad24fed4a2e14aa9affeb72ac2babc0b596
Michiel De Muynck <michieldemuynck@gmail.com>
2023-11-25 19:28:54 +0000

    Change profiles

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 python -m venv .venv
 source .venv/bin/activate
 pip install "dbt-core==1.4.9" "dbt-postgres==1.4.9" "sqlmesh[web,dbt,github,postgres]==0.56.4"
-sqlmesh init -t dbt
+
+
+# sqlmesh init -t dbt
